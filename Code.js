a7ca463de7d89c8a9ec3a3ede59e9bc5443b511b
Suchet-13 <75424875+Suchet-13@users.noreply.github.com>
2021-04-23 16:41:29 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white");
+    textSize(15);
+    text("O U C N F I T N",700,140);
+    fill("lightblue")
+    text("Hint: Set of statements that performs a task",700,160);
+
+    fill("white");
+    textSize(15);
+    text("E B A D S A T",100,350)
+    fill("lightblue")
+    text("Hint: An organised collection of data",100,370)
+
+}
